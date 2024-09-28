94f3e06301374f5ac6d4f7da0954571ad0a921a2
Cole Flannery <jaidencoleflannery@gmail.com>
2024-09-27 22:57:45 -0500

    DynamicArray.js - unfinished.

---
+++ DynamicArray.js
@@ -0,0 +1,107 @@
+class DynamicArray {
+    /*
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.arr = new Array(capacity);
+    }
+
+    /*
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i) {
+        return this.arr[i];
+    }
+
+    /*
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i, n) {
+        this.arr[i] = n;
+    }
+
+    /*
+     * @param {number} (n)
+     * @returns {void}
+     */
+    pushback(n) {
+        
+        if(this.getSize() == this.getCapacity()){
+            this.arr[this.arr.length - 1] = n;
+            return null;
+        }
+
+        for (let counter = this.arr.length - 1; counter >= 0; counter--) {
+            if (this.arr[counter] !== undefined) {
+                this.arr[counter + 1] = n;
+                break;
+            }
+        }
+    }
+
+    /*
+     * @returns {number}
+     */
+    popback() {
+        for (let counter = this.arr.length - 1; counter >= 0; counter--) {
+            if (this.arr[counter] !== undefined) {
+                let lastElement = this.arr[counter + 1];
+                this.arr[counter + 1] = undefined;
+                return lastElement;
+            } else if (counter == 0){
+                let lastElement = this.arr[this.arr.length - 1];
+                this.arr[this.arr.length - 1] = undefined;
+                return lastElement;
+            }
+        }
+    }
+
+    /*
+     * @returns {void}
+     */
+    resize() {
+        let doubleArr = new Array(this.arr.length * 2);
+
+        for(let count = 0; count < arr.length; count++){
+            doubleArr[count] = arr[count];
+        }
+        return doubleArr;
+    }
+
+    /*
+     * @returns {number}
+     */
+    getSize() {
+        let elements = 0;
+
+        if (!this.arr) {
+            return 0;
+        }
+
+        for (let count = 0; count < this.arr.length; count++) {
+            if(this.arr[count] != undefined){
+                elements++;
+            }
+        }
+
+        return elements;
+    }
+
+    /*
+    @returns {number}
+    */
+    getCapacity() {
+        let capacity = 0;
+
+        if(!this.arr){
+            return 0;
+        } else{
+            capacity = this.arr.length;
+        }
+        return capacity;
+    }
+}
