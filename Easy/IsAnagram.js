47d7669dbe354a5701e131bffe6fd3d9ae2aeb3e
Cole Flannery <jaidencoleflannery@gmail.com>
2024-11-06 12:23:54 -0600

    Finished IsAnagram.js

--- Easy/IsAnagram.js
+++ Easy/IsAnagram.js
@@ -1,29 +1,18 @@
-s = "racecar";
-t = "carrace";
-console.log(IsAnagram(s, t));
-
-function IsAnagram(s, t){
-    let count = 0;
-    const length = t.length;
-    let t2 = t;
-
-    for(let i = 0; i < s.length; i++){
-        for(let k = 0; k < t2.length; k++){
-            if(s[i] == t2[k]){
-                count++;
-                console.log('before: ', t2);
-                t2 = t2.slice(0, k) + t.slice((k + 1));
-                console.log('after: ', t2);
-            } else{
-                if(k == t2.length){
-                    console.log('falsieo');
-                }
-            }
-        }
-    }
-    if(count == length){
-        return true;
-    } else{
+var isAnagram = function(s, t) {
+    if(t.length !== s.length){
         return false;
     }
+
+    const countS = {};
+    const countT = {};
+    for(let i = 0; i < s.length; i++){
+        countS[s[i]] = (countS[s[i]] || 0) + 1;
+        countT[t[i]] = (countT[t[i]] || 0) + 1;
+    }
+    for(const key in countS){
+        if(countS[key] !== countT[key]){
+            return false;
+        }
+    }
+    return true;
 }
